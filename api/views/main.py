3d3ebcd5cbb4de143f826c750c30ec87857b2f70
Jin Young Bang <jybang@bu.edu>
2020-10-21 15:42:59 -0400

    flask boilerplate created

---
+++ api/views/main.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request
+# from api.models import db, Person, Email
+
+main = Blueprint("main", __name__)  # initialize blueprint
+
+
+# function that is called when you visit /
+@main.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+# function that is called when you visit /persons
+@main.route("/persons", methods=["GET"])
+def get_persons():
+    # persons = Person.query.all()
+    # return create_response(data={"persons": serialize_list(persons)})
+    pass
