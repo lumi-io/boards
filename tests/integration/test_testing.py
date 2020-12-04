5d6131d24b80f6fb26a9d38208493cafd58f0bb7
Jin Young Bang <jybang@bu.edu>
2020-12-04 17:52:02 -0500

    Implemented fixtures for pytest

--- tests/integration/test_testing.py
+++ tests/integration/test_testing.py
@@ -1,7 +1,3 @@
-from api import create_app
-
-def test_homepage():
-    app = create_app(test_config=True)
-    with app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
+def test_homepage_with_fixture(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
