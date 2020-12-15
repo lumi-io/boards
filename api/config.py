61d0c176e2f5c4adcae80e19f78e00aa9f399b34
Jin Young Bang <jybang@bu.edu>
2020-12-14 22:40:46 -0500

    edit on config

--- api/config.py
+++ api/config.py
@@ -10,15 +10,6 @@ MONGODB_PORT = int(os.getenv('MONGODB_PORT'))
 MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
 MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
 
-# class Config:
-#     """Base config."""
-#     MONGODB_DB = os.getenv('MONGODB_DB')
-#     MONGODB_HOST = os.getenv('MONGODB_HOST')
-#     MONGODB_PORT = os.getenv('MONGODB_PORT')
-#     MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
-#     MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
-
-
 # class ProdConfig(Config):
 #     FLASK_ENV = 'production'
 #     DEBUG = False
