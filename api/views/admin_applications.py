e03bb8afb763d06ac1cee89638e2ca71ac76b526
Jin Young Bang <jybang@bu.edu>
2020-12-23 18:57:54 +0900

    Creating template ffor APIs to work on

--- api/views/admin_applications.py
+++ api/views/admin_applications.py
@@ -8,6 +8,7 @@ from api import mongo, flask_bcrypt, jwt
 admin_applications = Blueprint(
     "admin_applications", __name__)  # initialize blueprint
 applications = mongo.db.applications
+postings = mongo.db.postings
 
 
 def return_exception(e):
@@ -18,44 +19,23 @@ def return_exception(e):
     return jsonify(response_object)
 
 
-@admin_applications.route('/admin/applications', methods=['GET'])
-def get_all_applications():
-    """ Endpoint that gets all titles to be read by the default page """
-    all_applications = []
-    try:
-        for application in applications.find():
-            all_applications.append(application)
-
-        response_object = {
-            "status": True,
-            "allApplications": all_applications,
-            "message": 'All postings received.'
-        }
-        return make_response(jsonify(response_object), 200)
-
-    except Exception as e:
-        return make_response(return_exception(e), 400)
+@admin_applications.route('/admin/postings/<posting_id>/applications', methods=['GET'])
+def read_all_applications(posting_id):
+    """ Endpoint that gets all applications of a posting/job """
+    pass
 
 
-@admin_applications.route('/admin/applications/<application_id>', methods=['GET'])
-def get_specific_application(application_id):
-    """ Endpoint that gets information of specific application based on id """
-    try:
-        application_info = applications.find_one(
-            {"_id": ObjectId(application_id)})
-        if not application_info:
-            response_object = {
-                "status": False,
-                "message": 'Application ID not found.'
-            }
-            return make_response(jsonify(response_object), 404)
+@admin_applications.route('/admin/postings/<posting_id>/applications/<applicant_id>', methods=['GET'])
+def read_specific_application(posting_id, applicant_id):
+    """ Endpoint that gets a specific application of a posting """
+    pass
 
-        response_object = {
-            "status": True,
-            "applicationInfo": application_info,
-            "message": 'Application found.'
-        }
-        return make_response(jsonify(response_object), 200)
+@admin_applications.route('/admin/postings/<posting_id>/applications/<applicant_id>', methods=['PATCH'])
+def edit_specific_application(posting_id, applicant_id):
+    """ Endpoint that edits a specific application of a posting """
+    pass
 
-    except Exception as e:
-        return make_response(return_exception(e), 400)
+@admin_applications.route('/admin/postings/<posting_id>/applications/<applicant_id>', methods=['DELETE'])
+def delete_specific_application(posting_id, applicant_id):
+    """ Endpoint that deletes a specific application of a posting """
+    pass
