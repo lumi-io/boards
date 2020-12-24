0484498072cb6ab7713aeffb21ba0cb16ba70e82
Jin Young Bang <jybang@bu.edu>
2020-12-24 12:07:07 +0900

    Added endpoint to manually update applicant detail

--- api/views/admin_applications.py
+++ api/views/admin_applications.py
@@ -51,7 +51,7 @@ def read_specific_application(posting_id, applicant_id):
                 }
 
                 return make_response(jsonify(response_object), 200)
-        
+
         response_object = {
             "status": True,
             "application": None,
@@ -73,4 +73,30 @@ def edit_specific_application(posting_id, applicant_id):
 @admin_applications.route('/admin/postings/<posting_id>/applications/<applicant_id>', methods=['DELETE'])
 def delete_specific_application(posting_id, applicant_id):
     """ Endpoint that deletes a specific application of a posting """
-    pass
+    try:
+        update_response = applications.update(
+            {"postingKey": ObjectId(posting_id)},
+            {"$pull": {
+                "applications": {
+                    "applicantId": ObjectId(applicant_id)
+                }
+            }}
+        )
+
+        if update_response["nModified"] == 0:
+            response_object = {
+                "status": True,
+                "message": "Delete unsuccessful. No application found with the id " + applicant_id + "."
+            }
+
+            return make_response(jsonify(response_object), 200)
+
+        response_object = {
+            "status": True,
+            "message": "Applicant " + applicant_id + " removed."
+        }
+
+        return make_response(jsonify(response_object), 200)
+
+    except Exception as e:
+        return make_response(return_exception(e), 400)
