c83188c2a14dc2e70b9d486cddf741c546de7822
Jin Young Bang <jybang@bu.edu>
2020-12-30 18:52:38 +0900

    Minor change on fixture, autouse parameter

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from api import create_app, mongo
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module", autouse=False)
 def test_client():
     app = create_app(test_config=True)
     with app.test_client() as testing_client:
