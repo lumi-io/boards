e01f0bf7e9a28717bff57f4572dbb0a963e92397
Jin Young Bang <jybang@bu.edu>
2020-12-30 22:41:57 +0900

    Added resending confirmation email endpoint

--- api/views/admin_auth.py
+++ api/views/admin_auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, make_response
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_required, jwt_refresh_token_required, get_jwt_identity)
-from api.validators.user import validate_user
+from api.validators.user import validate_user, validate_email
 from api import mongo, flask_bcrypt, jwt
 from api.middlewares.confirmation_email import send_confirmation_email
 import uuid
@@ -65,6 +65,7 @@ def register():
 
 @admin_auth.route('/confirmation/<confirmation_id>', methods=['GET'])
 def confirm_registration(confirmation_id):
+    """ Endpoint that verifies user's signup process through a confirmation ID from email """
     user = users.update(
         {"email_confirmation_id": confirmation_id},
         {
@@ -73,7 +74,71 @@ def confirm_registration(confirmation_id):
         },
         upsert=False
     )
-    return "Email confirmed"
+    if user["nModified"] == 0 and not user["updatedExisting"]:
+        response_object = {
+            "status": False,
+            "message": "Invalid ID. Please register a new user or resend the verification email."
+        }
+        return make_response(jsonify(response_object), 401)
+
+    response_object = {
+        "status": True,
+        "message": "Email verified."
+    }
+    return response_object(jsonify(response_object), 200)
+
+
+@admin_auth.route('/resend-confirmation', methods=['POST'])
+def resend_confirmation():
+    """ Endpoint that resends confirmation email to the user with a new ID """
+    data = validate_email(request.get_json())
+
+    if data['ok']:
+        data = data['data']
+        email = data["email"]
+
+        user = users.find_one({
+            "email": email
+        })
+
+        if not user:
+            response_object = {
+                "status": False,
+                "message": "Email not found. Please register a new account."
+            }
+            return make_response(jsonify(response_object), 400)
+        
+        if user["email_confirmed"]:
+            response_object = {
+                "status": False,
+                "message": "User has already been verified."
+            }
+            return make_response(jsonify(response_object), 400)
+
+        new_confirmation_id = str(uuid.uuid4())
+
+        user = users.update(
+            {"email": email},
+            {
+                "$set": {"email_confirmation_id": new_confirmation_id},
+            },
+            upsert=False
+        )
+        send_confirmation_email(email, new_confirmation_id)
+
+        response_object = {
+            "status": True,
+            "message": 'Resent verification/confirmation email.'
+        }
+        return make_response(jsonify(response_object), 200)
+
+    else:
+        response_object = {
+            "status": False,
+            "message": 'Bad request parameters: {}'.format(data['message'])
+        }
+        return make_response(jsonify(response_object), 400)
+
 
 
 @admin_auth.route('/admin/auth', methods=['POST'])
