851d9fade81ca31b1da16307e3873f0027eb255c
Jin Young Bang <jybang@bu.edu>
2020-12-30 18:44:54 +0900

    Created unit tests for Authentication and added DB drop functionality to module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
-from flask_pymongo import PyMongo
-from api import create_app
+from api import create_app, mongo
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def test_client():
     app = create_app(test_config=True)
     with app.test_client() as testing_client:
         with app.app_context():
             yield testing_client
+    mongo.db.users.delete_many({})
